2e6af763d7a13272e5fff3b7eae7e055f9b914b5
Gill Shen <gillarete@gmail.com>
2023-05-05 16:34:38 +0800

    Add a basic template

--- render.py
+++ render.py
@@ -1,20 +1,33 @@
+import dataclasses
 import jinja2
 
 ENVIRONMENT = jinja2.Environment(
-    block_start_string='<!',
-    block_end_string='!>',
-    variable_start_string='<<',
-    variable_end_string='>>',
-    comment_start_string='<#',
-    comment_end_string='#>',
+    block_start_string="<!",
+    block_end_string="!>",
+    variable_start_string="<<",
+    variable_end_string=">>",
+    comment_start_string="<#",
+    comment_end_string="#>",
     trim_blocks=False,
     lstrip_blocks=True,
     keep_trailing_newline=True,
 )
 
 
+@dataclasses.dataclass
+class Settings:
+    main_font: str
+    secondary_font: str
+    old_style_numbers: bool = True
+    font_size_in_point: int = 11
+    top_margin_in_inch: int = 0.8
+    bottom_margin_in_inch: int = 1
+    left_margin_in_inch: int = 1
+    right_margin_in_inch: int = 1
+
+
 def render(template_filename, **args):
-    with open(template_filename, encoding='utf-8') as template_file:
+    with open(template_filename, encoding="utf-8") as template_file:
         template_str = template_file.read()
     template = ENVIRONMENT.from_string(template_str)
     return template.render(**args)
--- test.py
+++ test.py
@@ -1,15 +1,15 @@
 from txtparse import parse
-from render import render
+from render import render, Settings
 
 
 def test_parse():
-    with open('tests/test_src.txt', encoding='utf-8') as f:
+    with open("tests/test_src.txt", encoding="utf-8") as f:
         cv = parse(f.read())
 
-    print(f'{cv.name=}')
-    print(f'{cv.email=}')
-    print(f'{cv.address=}')
-    print(f'{cv.phone=}')
+    print(f"{cv.name=}")
+    print(f"{cv.email=}")
+    print(f"{cv.address=}")
+    print(f"{cv.phone=}")
 
     for education in cv.education:
         print(education)
@@ -23,13 +23,22 @@ def test_parse():
 
 def test_render():
     args = {
-        'name': 'Alice',
-        'activities': ['hiking', 'rowing', 'digging graves'],
-        'awards': ['silliest walker', 'best mole']
+        "name": "Alice",
+        "activities": ["hiking", "rowing", "digging graves"],
+        "awards": ["silliest walker", "best mole"],
     }
-    print(render('tests/test_template.tex', **args))
+    print(render("tests/test_template.tex", **args))
 
 
-if __name__ == '__main__':
-    test_parse()
-    test_render()
+def test_parse_and_render():
+    settings = Settings(main_font="EB Garamond", secondary_font="EB Garamond")
+    with open("tests/test_src.txt", encoding="utf-8") as f:
+        cv = parse(f.read())
+    with open("tests/test_result.tex", "w", encoding="utf-8") as f:
+        f.write(render("templates/classic.tex", settings=settings, cv=cv))
+
+
+if __name__ == "__main__":
+    # test_parse()
+    # test_render()
+    test_parse_and_render()
